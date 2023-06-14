1ec22b65bead5ff7c7c300b0c523c38c0109147f
chow-q <73530205+chow-q@users.noreply.github.com>
2023-06-14 16:01:30 +0800

    Update ffmpeg-web-push.py

--- ffmpeg-web-push.py
+++ ffmpeg-web-push.py
@@ -7,13 +7,7 @@ import json
 import psutil
 app = Flask(__name__)
 
-# 用于存储推流任务的列表
-try:
-    # 从task.txt读取推流任务列表，并返回给前端
-    with open("tasks.txt", "r") as f:
-        stream_tasks = json.load(f)
-except Exception as e:
-    stream_tasks = []
+
 
 @app.route('/')
 def index():
@@ -53,12 +47,18 @@ def index():
                 </el-form>             
                 <hr/>
                 <h2>当前推流任务列表</h2>
-                <el-table :data="streamTasks" style="width: 100%">
-                  <el-table-column prop="file" label="回放视频"></el-table-column>
+                <el-table :data="streamTasks" :default-sort="{prop: 'duration'}" style="width: 100%">
+                  <el-table-column prop="file" label="回放视频" width="500"></el-table-column>
                   <el-table-column prop="pid" label="进程id"></el-table-column>
                   <el-table-column prop="times" label="播放次数"></el-table-column>
                   <el-table-column prop="status" label="状态"></el-table-column>
-                  <el-table-column prop="start_time" label="开始时间"></el-table-column>
+                  <el-table-column prop="duration" label="剩余播放时长"   align="center" width="120">
+                      <template slot-scope="scope">
+                        <el-statistic format="HH:mm:ss" :value="Date.now()+(scope.row.duration-(Date.now()-scope.row.start_time))" time-indices >
+                      </template>
+                  </el-table-column>
+                  <el-table-column prop="start_time" label="开始时间" :formatter="row=>new Date(row.start_time).toLocaleString()">
+                  </el-table-column>
                   <el-table-column prop="end_time" label="手动结束时间"></el-table-column>
                   <el-table-column label="操作">
                     <template slot-scope="scope">
@@ -74,7 +74,8 @@ def index():
                         formInline: {
                           file: '',
                           key: '',
-                          times: 1
+                          times: 1,
+                          duration: ''
                         },
                         rules: {
                           file: [{ required: true, message: '请输入推流文件', trigger: 'blur' }],
@@ -115,7 +116,8 @@ def index():
                               })
                               .then(response => {
                                 // 处理响应数据
-                                console.log(response.data);
+                                console.log(response.data.duration);
+                                console.log(Date.now() + 1000 * 60 * 60 * 8);
                                 // 弹窗展示数据
                                 this.$alert(response.data, '开始推流中，进程id：', {
                                   confirmButtonText: '确定',
@@ -124,9 +126,10 @@ def index():
                                 // 添加推流任务到列表
                                 var task = {
                                   file: this.formInline.file,
-                                  pid: response.data,
+                                  pid: response.data.pid,
                                   times: Number(this.formInline.times),
-                                  start_time: new Date().toLocaleString(),
+                                  duration: response.data.duration,
+                                  start_time: Date.now(),
                                   status: "正在推流中"
                                 }
                                 this.streamTasks.push(task);
@@ -173,24 +176,39 @@ def index():
 
 @app.route('/stream', methods=['POST'])
 def stream():
+    # 用于存储推流任务的列表
+    try:
+        # 从task.txt读取推流任务列表，并返回给前端
+        with open("tasks.txt", "r") as f:
+            stream_tasks = json.load(f)
+    except Exception as e:
+        stream_tasks = []
     action=request.get_json()['action']
+    duration="未知"
     if action == 'start':
         file=request.get_json()['file']
+        key=request.get_json()['key']
+        times=request.get_json()['times']
         # 共享盘的路径
-        shared_folder = r"\\192.168.0.1\共享盘"
+        shared_folder = r"\\192.168.0.202\共享盘"
         # 遍历共享盘及其子目录
         for dirpath, dirnames, filenames in os.walk(shared_folder):
             if file in filenames:
                 # 找到了文件，输出完整路径
                 file=os.path.join(dirpath, file)
+                # 调用FFmpeg获取视频元数据信息
+                result = subprocess.Popen(['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                output = result.communicate()[0]
+                # 解析输出中的时长信息
+                duration = int(float(output.decode('utf-8').split('duration": "')[1].split('",')[0])*times*1000)
                 break
-        key=request.get_json()['key']
-        times=request.get_json()['times']-1
-        times=str(times)
+        #推流的重复次数，在播放的次数的基础上-1就是重复的次数
+        times=str(times-1)
+        print(times)
         logs=key[0:22]+".txt"
         file=file.replace("\\","\\")
         #添加推流任务
-        process = subprocess.Popen(['ffmpeg', '-re', '-stream_loop', times, '-i', file, '-c', 'copy', '-progress', logs, '-f', 'flv', 'rtmp://test-push.live.com/live/' + key], creationflags=subprocess.CREATE_NEW_CONSOLE)
+        process = subprocess.Popen(['ffmpeg', '-re', '-stream_loop', times, '-i', file, '-c', 'copy', '-progress', logs, '-f', 'flv', 'rtmp://live-push.live.com/live/' + key], creationflags=subprocess.CREATE_NEW_CONSOLE)
         # 获取进程ID
         pid = process.pid
         print("子进程的进程ID为：", pid)
@@ -202,12 +220,13 @@ def stream():
             'pid': pid,
             'logs': logs,
             'status': '正在推流中',
-            'start_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'duration': duration,
+            'start_time': int(datetime.datetime.now().timestamp() * 1000),
             'end_time': ''
         }
         stream_tasks.append(task)
         save_tasks(stream_tasks)
-        return str(pid)
+        return {"pid": str(pid), "duration": duration}
     elif action == 'stop':
         pid=request.get_json()['pid']
         for task in stream_tasks:
@@ -226,11 +245,11 @@ def stream():
 def get_tasks():
     # 从task.txt读取推流任务列表，并返回给前端
     if not os.path.isfile("tasks.txt"):
-         with open("tasks.txt", "w") as f:
-             pass
+        with open("tasks.txt", "w") as f:
+            pass
     try:
         # 从task.txt读取推流任务列表，并返回给前端
-        with open("tasks.txt", "r") as f:
+        with open("tasks.txt", "r+") as f:
             stream_tasks = json.load(f)
             for task in stream_tasks:
                 process_exists = psutil.pid_exists(task['pid'])
@@ -251,4 +270,4 @@ def save_tasks(stream_tasks):
         json.dump(stream_tasks, f)
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(host='0.0.0.0', port=80)
